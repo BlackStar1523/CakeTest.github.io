0ffaafe99cea344a74702df152f3c5be1f1d5844
BlackStar1523 <djankoufamily@gmail.com>
2025-08-20 22:08:26 +0200

    Update main.js

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbw6qu6FrM57cTXG0yRiuWN4iuQ7km98h6QxKTy5-3hlPE952y371FVMwWxUc168nWf/exec",
   CLOUDINARY_CLOUD_NAME: "dk0ioppgv",
-  CLOUDINARY_UPLOAD_PRESET: "Cake_Test",   // <-- le preset qui a marché en console
+  CLOUDINARY_UPLOAD_PRESET: "cake_test",   // <-- le preset qui a marché en console
   MAX_IMG: 1600,
 };
 
