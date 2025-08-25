e215e5292b9a6e09ac0d866d17a97f9e9dfdab01
Lindrich <djankoufamily@gmail.com>
2025-08-25 21:48:32 +0200

    Update and rename config.prod.js to config.js

---
+++ assets/JS/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbw6qu6FrM57cTXG0OyRiuWN4iuQ7km98h6QxKTy5-3hlPE952y371FVMwWxUc168nWf/exec",
+  CLOUDINARY_CLOUD_NAME: "dk0ioppgv",
+  CLOUDINARY_UPLOAD_PRESET: "Cake_Test",
+  MAX_IMG: 1600,
+};
