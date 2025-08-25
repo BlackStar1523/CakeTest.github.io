d4052bfa971825e736111a1c8c53f4bb49bfddbd
Lindrich <djankoufamily@gmail.com>
2025-08-25 18:25:00 +0200

    Update config.prod.js

--- assets/JS/config.prod.js
+++ assets/JS/config.prod.js
@@ -1,1 +1,7 @@
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbxwbDIA6vgkH7i5Ml7CfKHWtP2a4I967wrjAqq66TxcHL9wuKfPugLfaviUWjNcZZ2o/exec",
+  CLOUDINARY_CLOUD_NAME: "dk0ioppgv",
+  CLOUDINARY_UPLOAD_PRESET: "CakeTest",
+  MAX_IMG: 1600,
+};
 
