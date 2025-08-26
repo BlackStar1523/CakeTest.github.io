220a9f6438a1fb44f5182fda46cc7512f2dc8786
Lindrich <djankoufamily@gmail.com>
2025-08-26 18:44:00 +0200

    Update config.js

--- assets/JS/config.js
+++ assets/JS/config.js
@@ -1,6 +1,7 @@
 window.CONFIG = {
   ENV: "TEST",
-  API_URL: "https://script.google.com/macros/s/AKfycbw6qu6FrM57cTXG0OyRiuWN4iuQ7km98h6QxKTy5-3hlPE952y371FVMwWxUc168nWf/exec",
+  //API_URL: "https://script.google.com/macros/s/AKfycbw6qu6FrM57cTXG0OyRiuWN4iuQ7km98h6QxKTy5-3hlPE952y371FVMwWxUc168nWf/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbysuhc2mWZNn2utxwF6InS7YLFU76JSkVkfOih-eesQE5jbuqM5Zrh4ImNX6kTVeMoA/exec",
   CLOUDINARY_CLOUD_NAME: "dk0ioppgv",
   CLOUDINARY_UPLOAD_PRESET: "Cake_Test",
   MAX_IMG: 1600
